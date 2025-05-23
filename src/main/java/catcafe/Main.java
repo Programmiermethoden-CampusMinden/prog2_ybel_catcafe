fe908d1d697e19b5a2204ad36e5465456b142895
Phoby22 <wesslingpatrick@gmx.de>
2025-05-23 16:56:35 +0200

    change call of Methods with return values Optional

--- src/main/java/catcafe/Main.java
+++ src/main/java/catcafe/Main.java
@@ -17,13 +17,16 @@ public class Main {
 
         System.out.println("Es schnurren " + cafe.getCatCount() + " Samtpfötchen.");
 
-        FelineOverLord meow = cafe.getCatByWeight(3, 4);
-        if (meow != null) System.out.println("Gewicht [3,4]: " + meow);
+        cafe.getCatByWeight(3, 4).ifPresent(cat ->
+            System.out.println("Gewicht [3,4]: " + cat)
+        );
 
-        meow = cafe.getCatByName("Morticia");
-        if (meow != null) System.out.println("Name 'Morticia': " + meow);
+        cafe.getCatByName("Morticia").ifPresent(cat ->
+            System.out.println("Name 'Morticia': " + cat)
+        );
 
-        meow = cafe.getCatByName("Miss Chief Sooky");
-        if (meow != null) System.out.println("Name 'Miss Chief Sooky': " + meow);
+        cafe.getCatByName("Miss Chief Sooky").ifPresent(cat ->
+            System.out.println("Name 'Miss Chief Sooky': " + cat)
+        );
     }
 }
