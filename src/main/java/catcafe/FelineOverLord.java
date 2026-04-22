7e8fc095b74ee0176fd440c45ca41e118a1050fe
Carsten Gips <cagix@hsbi.de>
2026-04-22 11:40:52 +0200

    we did not cover records so far - converting to plain old class

--- src/main/java/catcafe/FelineOverLord.java
+++ src/main/java/catcafe/FelineOverLord.java
@@ -3,12 +3,40 @@ package catcafe;
 /**
  * A purring feline overlord.
  *
- * @param name name of the cat
- * @param weight weight of the cat
  */
-public record FelineOverLord(String name, int weight) implements Comparable<FelineOverLord> {
+public final class FelineOverLord implements Comparable<FelineOverLord> {
+    private final String name;
+    private final int weight;
+
+    /**
+     * Create a new cat.
+     *
+     * @param name   name of the cat
+     * @param weight weight of the cat
+     */
+    public FelineOverLord(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public int weight() {
+        return weight;
+    }
+
     @Override
     public int compareTo(FelineOverLord o) {
         return weight - o.weight;
     }
+
+    @Override
+    public String toString() {
+        return "FelineOverLord[" +
+            "name=" + name + ", " +
+            "weight=" + weight + ']';
+    }
+
 }
